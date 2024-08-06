5fdd89e48d8b08df83efa2877beae5b99ccbdd2d
exiondew <exiondew@gmail.com>
2024-08-06 11:46:42 +0300

    başlıyoruz

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <div className="bg-green-400 text-white text-4xl py-4 text-center font-medium">
+        Hello World
+      </div>
+    </div>
+  );
+}
+
+export default App;
